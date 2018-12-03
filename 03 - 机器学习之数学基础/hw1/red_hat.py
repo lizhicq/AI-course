b524ac11874b9b32db8b5dc5bd7d7ef3470faff7
Zhi Li <lizhicq@gmail.com>
2018-12-02 19:23:03 -0500

    redhat hw1

--- 03 - 机器学习之数学基础/hw1/red_hat.py
+++ 03 - 机器学习之数学基础/hw1/red_hat.py
@@ -1,2 +1,13 @@
-import pandas as pd
-import numpy as np
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+logger.info('This is a log info')
+
+logger.debug('Debugging')
+
+logger.warning('Warning exists')
+
+logger.info('Finish')
